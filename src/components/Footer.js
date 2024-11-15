1c16421148e191e726a6a770e08e9aff1ead5ae8
sakshi <sakshikatkar2020@gmail.com>
2024-11-15 10:31:37 +0530

    electronic

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-dark text-white text-center p-3 mt-3 fs-5'>
+      <p>Copyright &copy; 2024 E-Commerce Wesite. Built with React.</p>
+    </div>
+  )
+}
+
+export default Footer
